dbb5f39e7fc626a159fe70f7423cfe93514831d5
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-01-03 19:12:28 +0000

    first commit

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
 
+    get '/landmarks' do
+        erb :'/landmarks/index'
+    end
+    
+    get '/landmarks/new' do
+        erb :'/landmarks/new'
+    end
+    
+    post '/landmarks' do
+        @landmark = Landmark.create(params[:landmark])
+        @landmark.save
+        erb :'/landmarks/show'
+    end
+    
+    get '/landmarks/:id' do
+       @landmark = Landmark.find(params[:id])
+       erb :'/landmarks/show'
+    end
+    
+    get '/landmarks/:id/edit' do
+        @landmark = Landmark.find(params[:id])
+        erb :'/landmarks//edit'
+    end
+
 end
